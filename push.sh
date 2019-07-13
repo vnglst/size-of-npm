7841296f54273566a25692dc12aa06b56dc70bfa
Koen van Gilst <koen.vangilst@gmail.com>
2019-07-13 22:57:50 +0200

    adds git checkout

--- push.sh
+++ push.sh
@@ -5,6 +5,7 @@ du -sh node_modules >> size.txt
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git checkout $TRAVIS_PULL_REQUEST_BRANCH
 git add --all
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
