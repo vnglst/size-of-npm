b8f416aa33b2c1dfd6f5154ab92ce1e83b417f9b
Koen van Gilst <koen.vangilst@gmail.com>
2019-07-13 23:22:11 +0200

    travis branch

--- push.sh
+++ push.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-du -sh node_modules >> size.txt
-
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git clone https://github.com/vnglst/size-of-npm.git
-git checkout $TRAVIS_PULL_REQUEST_BRANCH
+echo ${TRAVIS_BRANCH}
+git checkout ${TRAVIS_BRANCH}
+
+du -sh node_modules >> size.txt
+
 git add --all
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git push
+git push https://${GH_TOKEN}github.com/vnglst/size-of-npm.git
