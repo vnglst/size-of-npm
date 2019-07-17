df59c4577b2f7b9b0f4f2b8f8c0024f4752ed6e8
Koen van Gilst <koen.vangilst@gmail.com>
2019-07-17 15:44:04 +0200

    update path size.json

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./App.css"
 import { LineChart } from "react-chartkick"
 import "chart.js"
-import size from "../size.json"
+import size from "./size.json"
 
 function App() {
   return (
--- size/tojson.js
+++ size/tojson.js
@@ -5,7 +5,7 @@ function parse(data) {
   lines.pop()
   const result = {}
   lines.forEach(line => {
-    const key = line.split(" ")[0]
+    const key = line.split(" ")[0].replace(".", " ")
     const value = line.split(" ")[1]
     result[key] = value
   })
@@ -16,7 +16,7 @@ async function main() {
   const data = await fs.readFile("./size.txt", "utf8")
   const json = parse(data)
   const jsonString = JSON.stringify(json, null, 4)
-  await fs.writeFile("../app/size.json", jsonString, "utf8")
+  await fs.writeFile("../app/src/size.json", jsonString, "utf8")
   console.log("Done!")
 }
 
