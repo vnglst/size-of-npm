5bcc4b420e42bf8e05708437db1d74aa8c960691
Koen van Gilst <koen.vangilst@gmail.com>
2019-07-17 10:44:25 +0200

    use circle ci

--- push.sh
+++ push.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
-echo ${TRAVIS_BRANCH}
-git checkout ${TRAVIS_BRANCH}
-
-du -sh node_modules >> size.txt
+git config --global user.email "ci@koenvangilst.nl"
+git config --global user.name "Circle CI"
 
 git add --all
-git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git commit --message "[skip ci] Circle CI: add line for size"
 
 git push https://${GH_TOKEN}github.com/vnglst/size-of-npm.git
