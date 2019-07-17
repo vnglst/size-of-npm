7a650b3fa95af63f409a9af79bda361ddbce053f
Koen van Gilst <koen.vangilst@gmail.com>
2019-07-17 15:25:16 +0200

    add basic web app

---
+++ size/tojson.js
@@ -0,0 +1,23 @@
+const fs = require("fs").promises
+
+function parse(data) {
+  const lines = data.split("M\n")
+  lines.pop()
+  const result = {}
+  lines.forEach(line => {
+    const key = line.split(" ")[0]
+    const value = line.split(" ")[1]
+    result[key] = value
+  })
+  return result
+}
+
+async function main() {
+  const data = await fs.readFile("./size.txt", "utf8")
+  const json = parse(data)
+  const jsonString = JSON.stringify(json, null, 4)
+  await fs.writeFile("../app/size.json", jsonString, "utf8")
+  console.log("Done!")
+}
+
+main()
